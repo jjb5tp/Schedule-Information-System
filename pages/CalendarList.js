03dcff2ed1eb5fb8fd3af1df1f4e3ee03ba9beac
Jonathan Burkher <jjb5tp@virginia.edu>
2020-12-01 23:34:02 -0500

    touching up

--- pages/CalendarList.js
+++ pages/CalendarList.js
@@ -54,11 +54,11 @@ class CalendarsList extends Component {
         <Header title = "Calendar View" navigation = {this.props} addbutton = {true}/>
         <CalendarList
           testID={testIDs.calendarList.CONTAINER}
-          current={'2020-06-10'}
-          pastScrollRange={24}
-          futureScrollRange={24}
+          current={'2020-12-02'}
+          pastScrollRange={69}
+          futureScrollRange={69}
           renderHeader={(date) => {
-            const header = date.toString('MMMM yyyy');
+            const header = date.toString('MMMM, yyyy');
             const [month, year] = header.split(' ');
             const textStyle = {
               fontSize: 18,
@@ -73,12 +73,11 @@ class CalendarsList extends Component {
               <View style={{
                 flexDirection: 'row',
                 width: '100%',
-                justifyContent: 'space-between',
+                justifyContent: 'center',
                 marginTop: 10,
                 marginBottom: 10
               }}>
-              <Text style={{marginLeft: 5, ...textStyle}}>{`${month}`}</Text>
-              <Text style={{marginRight: 5, ...textStyle}}>{year}</Text>
+              <Text style={textStyle}>{`${header}`}</Text>
               </View>
             );
           }}
--- pages/CalendarScreen original.js
+++ pages/CalendarScreen original.js
@@ -396,6 +396,7 @@ const CalendarsScreen = () => {
   return (
     <ScrollView showsVerticalScrollIndicator={false} testID={testIDs.calendars.CONTAINER}>
       {renderCalendarWithSelectableDate()}
+      {renderCalendarWithWeekNumbers()}
       {renderCalendarWithMarkedDatesAndHiddenArrows()}
       {renderCalendarWithPeriodMarkingAndSpinner()}
       {renderCalendarWithPeriodMarkingAndDotMarking()}
